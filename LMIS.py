4536b080b1949bca1cc2cac2e2a7c78e5d2a2c14
Shafiranz940 <shafiranz240905@gmail.com>
2025-12-12 23:04:30 +0700

    Update LMIS.py

--- LMIS.py
+++ LMIS.py
@@ -1,28 +1,41 @@
-input_angka = [4, 1, 13, 7, 0, 2, 8, 11, 3]
+class LMISFinder:
+    def __init__(self, input_list):
+        self.arr = input_list
+        self.max_len = 0
+        self.all_best = []
 
-semuaBest_list = []      
-semuaBest_len = 0       
+    def dfs(self, index, current, depth):
+        COLORS = [
+            "\033[91m", "\033[92m", "\033[93m", "\033[94m",
+            "\033[95m", "\033[96m", "\033[97m"
+        ]
+        RESET = "\033[0m"
 
-def dfs(index, current, depth):
-    global semuaBest_len, semuaBest_list
+        color = COLORS[depth % len(COLORS)]
+        print("  " * depth + color + f"{current}" + RESET)
 
-    print("  " * depth + f"{current}")
+        if len(current) > self.max_len:
+            self.max_len = len(current)
+            self.all_best = [current[:]]
+        elif len(current) == self.max_len:
+            self.all_best.append(current[:])
 
-    if len(current) > semuaBest_len:
-        semuaBest_len = len(current)
-        semuaBest_list = [current[:]]     
-    elif len(current) == semuaBest_len:
-        semuaBest_list.append(current[:]) 
+        for i in range(index, len(self.arr)):
+            if not current or self.arr[i] > current[-1]:
+                self.dfs(i + 1, current + [self.arr[i]], depth + 1)
 
-    for i in range(index, len(input_angka)):
-        if not current or input_angka[i] > current[-1]:
-            dfs(i + 1, current + [input_angka[i]], depth + 1)
+    def find_all_lis(self):
+        self.dfs(0, [], 0)
+        return self.all_best, self.max_len
 
 
-dfs(0, [], 0)
+if __name__ == "__main__":
+    input_angka = [4, 1, 13, 7, 0, 2, 8, 11, 3]
 
-print("\n============================")
-print("Jumlah semua LIS dengan panjang maksimum =", semuaBest_len)
-for lis in semuaBest_list:
+    finder = LMISFinder(input_angka)
+    all_lis, panjang = finder.find_all_lis()
 
-    print(lis)
+    print("\n============================")
+    print("Jumlah semua LMIS dengan panjang maksimum =", panjang)
+    for lis in all_lis:
+        print(lis)
